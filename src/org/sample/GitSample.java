180eb5fd388e9c85ec2bfc5cf05d7c80edb1e08f
MohamadS85 <mohamadsst85@gmail.com>
2025-08-29 17:52:20 +0530

    projectAuDone

---
+++ src/org/sample/GitSample.java
@@ -0,0 +1,18 @@
+package org.sample;
+
+public class GitSample {
+
+	
+	
+	public static void main(String[] args) {
+		
+	int a = 4343; 
+	System.out.println("A value : "+a);
+	
+	System.out.println("Employee Details");
+	
+	
+		
+		
+	}
+}
